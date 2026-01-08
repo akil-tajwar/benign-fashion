e89aa0d1a3352872747a051225813d6f7594db39
Mozahad1563 <mozahad@brainstation-23.com>
2026-01-08 23:02:28 +0600

    CORS added

---
+++ infra/mysql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS benign_fashion CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER IF NOT EXISTS 'devuser'@'%' IDENTIFIED BY 'DevPass@123';
+GRANT ALL PRIVILEGES ON benign_fashion.* TO 'devuser'@'%';
+FLUSH PRIVILEGES;
